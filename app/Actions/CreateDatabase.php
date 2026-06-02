e278b17ead380b3506badf0256648e1c3dd3bb86
unknown <jefmaion@hotmail.com>
2026-06-02 20:41:10 -0300

    wip

--- app/Actions/CreateDatabase.php
+++ app/Actions/CreateDatabase.php
@@ -14,6 +14,7 @@ class CreateDatabase
     public static function run(Tenant $tenant)
     {
         Config::set('database.connections.tenant.database', $tenant->database);
+        Config::set('database.connections.tenant.username', env('DB_PREFIX').'_'.$tenant->subdomain);
 
         // DB::statement("CREATE DATABASE " . $tenant->database);
 
